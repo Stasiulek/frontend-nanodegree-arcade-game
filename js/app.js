9c28139563ac80fe6793e6db5eb100dad63e8464
Stanislaw Krzyzewski <s.krzyzewski@gmail.com>
2019-01-16 22:05:48 +0000

    [FUNCTIONALITY] Enemy movement loop added

--- js/app.js
+++ js/app.js
@@ -16,7 +16,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += 101 * dt;
+    if (this.x < 503){
+        this.x += 101 * dt;
+    } else {
+        this.x = -101;
+    }
+    
 };
 
 // Draw the enemy on the screen, required method for game
